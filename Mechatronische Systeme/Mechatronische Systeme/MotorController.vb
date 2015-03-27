bc218c8896763a0aa7bed5aa3a904b4543558dd4
René Lottes <rene.lottes@hof-university.de>
2015-03-27 08:05:13 +0100

    MotorController.vb
    Die ersten leeren Methoden für den Motorkontroller
    es kommen warscheinlich noch ein paar private hinzu
    es fehlt noch der Thread

--- Mechatronische Systeme/Mechatronische Systeme/MotorController.vb
+++ Mechatronische Systeme/Mechatronische Systeme/MotorController.vb
@@ -1,3 +1,23 @@
 ﻿Public Class MotorController
+	Private datas As List(Of Integer())
+	Private max_steps_x As Integer
+	Private max_steps_y As Integer
+	Private max_x As Integer 'die cm des blattes warscheinlich A4
+	Private max_y As Integer
 
+	'konstruktor als singelton
+
+	Private Sub reset() 'reset Motoren, zähle die steps für einmal komplett ausfahren
+	End Sub
+	
+	Public Sub setDatas(datas As List(Of Integer())) ' bekommt das Daten-Array mit den Linien und Stift zuständen
+		'berechnet anhand der koordinaten des Arrays dir anzahl der Steps und konvertiert somit die cm in steps für die motorsteuerung
+	End Sub
+
+	Public function drawNext() As Boolean 'solange Bool=true, hat noch ein datensatz im Array und kann weiter zeichnen
+	End function
+	
+	Private Sub move(ByVal x_steps As Integer, ByVal y_steps As Integer, ByVal status As Boolean)
+		'bewegt den Stift in x,y richtig mit den angegebenen Steps so, dass die Motoren unterschiedlich schnell laufen damit die Endposition gleichzeitig erreicht wird
+	End Sub
 End Class
