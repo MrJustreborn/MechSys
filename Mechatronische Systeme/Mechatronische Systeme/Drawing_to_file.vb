832188698bd4799264eb076f74474b45d29b11d2
philipp.wanninger <pwanninger@hof-university.de>
2015-05-25 09:19:45 +0200

    Kommentieren fortgesetzt

--- Mechatronische Systeme/Mechatronische Systeme/Drawing_to_file.vb
+++ Mechatronische Systeme/Mechatronische Systeme/Drawing_to_file.vb
@@ -3,21 +3,23 @@
 Public Class Drawing_to_file
     Private p_end As Point
     Private line_insert As Integer = 0
-
     Private last_drawing_steps As Integer = 0
-    Private insert_steps_list As List(Of Integer)
     Private write As Boolean = False
     Private fileWriter As IO.StreamWriter
     Private _path As String = Application.StartupPath + "\temp.txt"
     Private Shared instance As Drawing_to_file
 
 
-
+    'In diesem privaten Konstruktur wird durch Aufruf der Funktion "createFile" eine
+    'Datei angelegt
     Private Sub New()
         Me.createFile()
-        Me.insert_steps_list = New List(Of Integer)
     End Sub
 
+    ' Da es sich hier bei dieser Klasse um ein "Singleton - Pattern" handelt, besitzt diese auch keinen
+    ' öffentlich zugänglichen Konstruktur; stattdessen wird durch Aufruf dieser Funktion eine Instanz 
+    ' zurueckgegeben; hierbei wird zunaechst ueberprueft, ob diese bereits existiert; falls nicht wird 
+    ' eine entsprechende angelegt
     Public Shared ReadOnly Property getInstance() As Drawing_to_file
         Get
             If IsNothing(instance) Then
@@ -27,6 +29,7 @@ Public Class Drawing_to_file
         End Get
     End Property
 
+    'Bei Aufruf dieser Funktion wird eine leere,temporaere Datei angelegt
     Private Sub createFile()
         Dim fileStream As New IO.FileStream(Me._path, IO.FileMode.OpenOrCreate, IO.FileAccess.Write, IO.FileShare.None)
 
@@ -34,32 +37,34 @@ Public Class Drawing_to_file
 
     End Sub
 
+    'Diese Funktion sorgt dafuer, die erforderlichen Funktionen aufzurufen, die noetig sind, um die Daten fuer
+    'eine Linie in der temporaeren Datei abzuspeichern; die dafuer einzutragenden Zeilen werden in der Variablen
+    '"line_insert" abgelegt
     Public Sub save_drawing_steps(ByVal p_start As Point, ByVal p_end As Point)
         Me.last_drawing_steps = Me.line_insert
         Me.line_insert = 0
         Me.write_no_drawed_line(p_start)
         Me.write_drawed_line(p_end)
-        '       Me.line_insert += 1
         Me.line_insert += 1
-        '        Me.insert_steps_list.Add(Me.line_insert)
-        'Me.line_insert = 0
         Me.p_end = p_end
         
     End Sub
 
+    'Diese Funktion sorgt dafuer, die erforderlichen Funktionen aufzurufen, die noetig sind, um die Daten fuer
+    'einen Kreis in der temporaeren Datei abzuspeichern; die dafuer einzutragenden Zeilen werden in der Variablen
+    '"line_insert" abgelegt
     Public Sub save_drawing_steps(ByVal middle As Point, ByVal point As Point, ByVal swapAngle As Integer)
         Me.last_drawing_steps = Me.line_insert
         Me.line_insert = 0
         Me.write_no_drawed_line(point)
         Me.write_drawed_circle(middle, swapAngle)
         Me.line_insert += 1
-        '     Me.line_insert += 1
-        '    Me.insert_steps_list.Add(Me.line_insert)
-        ' Me.line_insert = 0
         Me.p_end = point
         
     End Sub
 
+    'Bei Aufruf dieser Funktion wird in der temporaeren Datei, die erforderlichen Zeilen hinzugefuegt,
+    ' die noetig sind, um einen Kreis, die der Drucker mit abgesetzten Stift, entlangfahren muss
     Private Sub write_drawed_circle(ByVal middle As Point, ByVal swapAngle As Integer)
         Dim txt As String
         Me.fileWriter = My.Computer.FileSystem.OpenTextFileWriter(Me._path, True)
@@ -68,6 +73,8 @@ Public Class Drawing_to_file
         Me.fileWriter.Close()
     End Sub
 
+    'Bei Aufruf dieser Funktion wird in der temporaeren Datei, die erforderlichen Zeilen hinzugefuegt,
+    ' die noetig sind, um eine Linie, die der Drucker mit abgesetzten Stift, entlangfahren muss
     Private Sub write_drawed_line(ByVal point As Point)
         Dim txt As String
         Me.fileWriter = My.Computer.FileSystem.OpenTextFileWriter(Me._path, True)
@@ -76,6 +83,8 @@ Public Class Drawing_to_file
         Me.fileWriter.Close()
     End Sub
 
+    'Bei Aufruf dieser Funktion wird in der temporaeren Datei, die erforderlichen Zeilen hinzugefuegt,
+    ' die noetig sind, um eine Linie, die der Drucker mit angehobenen Stift, entlangfahren muss
     Private Sub write_no_drawed_line(ByVal point As Point)
         Dim txt As String
 
@@ -88,7 +97,6 @@ Public Class Drawing_to_file
             txt = "PD"
             Me.fileWriter.WriteLine(txt)
             Me.fileWriter.Close()
-
             Me.p_end = point
             Me.line_insert += 3
 
@@ -96,12 +104,15 @@ Public Class Drawing_to_file
 
     End Sub
 
+    'Bei Aufruf dieser Funktion wird die Datei, falls vorhanden, im angegebenen Pfad (_path) geloescht
     Public Sub delete_file()
         If File.Exists(Me._path) Then
             My.Computer.FileSystem.DeleteFile(Me._path)
         End If
     End Sub
 
+    'Bei Aufruf dieser Funktion wird eine Kopie der temporaeren Datei "temp" erstellt und die Kopie im
+    'uebergebenen Pfad abgelegt; die temporaere Datei wird zum Abschluß geloescht
     Public Sub save_file_to_path(ByVal filename As String)
         If File.Exists(Me._path) Then
             File.Copy(Me._path, filename, True)
@@ -109,22 +120,20 @@ Public Class Drawing_to_file
         End If
     End Sub
 
+    'Bei Betaetigung des Buttons "back" in der Form "DrawingView" wird diese Funktion aufgerufen;
+    'hierbei wird die temporaer abgespeicherte Datei "temp" eingelesen, der Inhalt in dieser Datei
+    'anschließend geloescht und die eingelesenen Daten bis auf die letzte Zeile/n wieder eingetragen
+    'bei einem Fehler wird eine entsprechende Meldung ausgegeben
     Public Sub back()
-        'Dim tmp As Integer 'letzter Wert im insert_steps_array
         Dim insert_lines() As String  'speichert die eingefügten Zeilen aus der temp - Datei
         Dim list As New List(Of String)
         Dim writer As StreamWriter
 
         Me.last_drawing_steps = Me.line_insert
 
-
         Try
             insert_lines = IO.File.ReadAllLines(Me._path)
             list = insert_lines.ToList()
-
-            Console.WriteLine(Me.last_drawing_steps)
-            Console.WriteLine(list.Count)
-            Console.WriteLine(Me.line_insert)
             If (Me.last_drawing_steps > 0 And Me.last_drawing_steps <= list.Count) Then
                 Me.delete_file()
                 Me.createFile()
@@ -143,6 +152,8 @@ Public Class Drawing_to_file
 
     End Sub
 
+    'Diese Funktion gibt den Pfad zurueck, in der die Druckdatei, welche gezeichnet worden ist
+    'abgelegt wird
     Public Function getPath() As String
         Return Me._path
     End Function
--- Mechatronische Systeme/Mechatronische Systeme/MainView.vb
+++ Mechatronische Systeme/Mechatronische Systeme/MainView.vb
@@ -4,7 +4,9 @@
     Private moCon As MotorController
     Private plotterPause As Boolean = True
 
-
+    'Bei Aufruf dieser Funktion wird zunaechst ein Dialogfenster angezeigt, bei dem der Benutzer die Druckerdatei auswaehlen 
+    'kann; ist dies geschehen, so wird im Controller die "showPreview" Funktion aufgerufen, die dazu führt,
+    'dass in der Preview des Hauptfensters eine Druckvorschau angezeigt wird
     Private Sub DruckerdateiLadenToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ToolStripMenuItm_1.Click
         If (Opn_filepath.ShowDialog() = System.Windows.Forms.DialogResult.OK) Then
             Me.Refresh()
@@ -14,10 +16,16 @@
         End If
     End Sub
 
+    'Durch Betaetigung des Buttons "Btn_start" wird diese Funktion aufgerufen;
+    'Diese ruft die Funktion "start_plotter" auf und startet somit den Drucker
     Private Sub Btn_start_Click(sender As System.Object, e As System.EventArgs) Handles Btn_start.Click
         Me.start_plotter()
     End Sub
 
+    'Diese Funktion startet den Druckvorgang des Druckers; dazu wird zunaechst ueberprueft, ob eine 
+    ' Druckdatei bereits ausgewaehlt worden ist; ist dies der Fall so wird der Druckvorgang gestartet,
+    ' sowie die entsprechenden Buttons aktiviert/deaktiviert; ist keine Druckdatei vorher ausgewaehlt worden
+    ' so wird eine entsprechende Meldung ausgegeben
     Public Sub start_plotter()
         If Not (filepath Is Nothing) Then
 
@@ -31,35 +39,42 @@
         End If
     End Sub
 
+    'Bei Betaetigung des Buttons in der Menueleiste wird die Form "Settings" geladen und angezeigt
     Private Sub EinstellungenToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ToolStripMenuItm_2.Click
         Settings.Show()
     End Sub
 
+    'Diese Funnktion ist zustaendig, um die Button "Btn_pause", sowie "Btn_start" abwechselnd zu aktivieren/deaktivieren
     Private Sub switch_disable_buttons()
         Me.Btn_pause.Enabled = Not (Me.Btn_pause.Enabled)
         Me.Btn_start.Enabled = Not (Me.Btn_start.Enabled)
     End Sub
-  
+
+    'Diese Funktion deaktiviert die beiden Buttons "ToolStripMenuItem_1", sowie "ToolStripMenuItem_2"
     Private Sub disable_ToolStripItm()
         Me.ToolStripMenuItm_1.Enabled = False
         Me.ToolStripMenuItm_2.Enabled = False
     End Sub
 
+    'Diese Funktion aktiviert die beiden Buttons "ToolStripMenuItem_1", sowie "ToolStripMenuItem_2"
     Private Sub enable_ToolStripItm()
         Me.ToolStripMenuItm_1.Enabled = True
         Me.ToolStripMenuItm_2.Enabled = True
     End Sub
    
-
+    'Diese Funktion dient dazu, Linien/Graphiken in der "Preview" zu zeichnen
     Public Sub draw_preview(ByVal p_old As Point, ByVal p_new As Point)
 
         Me.Group_1.CreateGraphics.DrawLine(Pens.White, p_old.X, p_old.Y, p_new.X, p_new.Y)
     End Sub
 
+    'Diese Funktion dient dazu, Linien/Graphiken im "Live Printing" zu zeichnen
     Public Sub draw_live_print(ByVal p_old As Point, ByVal p_new As Point)
         Me.Group_2.CreateGraphics.DrawLine(Pens.White, p_old.X, p_old.Y, p_new.X, p_new.Y)
     End Sub
 
+    'Bei Betaetigung des Buttons "Btn_pause" wird der Druckvorgang pausiert, sowie entsprechende Buttons
+    'fuer ein Fortfahren des Vorgang aktiviert und entsprechend beschriftet
     Private Sub Btn_pause_Click(sender As System.Object, e As System.EventArgs) Handles Btn_pause.Click
         If (Me.plotterPause) Then
             '  Me.con.unpause_plotter()
@@ -71,6 +86,9 @@
         Me.plotterPause = Not Me.plotterPause
     End Sub
 
+    'Der Fortschritt des Druckvorgangs wird durch diese Funktion gesteuert - die Progressbar "Progressbar"
+    'kann hier je nach Fortschritt des Druckvorgangs auf einen bestimmten Wert eingestellt werden
+    'zusaetzlich wird der Wert im Label "lbl_progress" angezeigt 
     Public Sub Progress(ByVal value As Integer)
         If (Progress_1.Value < 101) Then
             Me.Progress_1.Value += value
@@ -79,31 +97,31 @@
 
     End Sub
 
+    'Beim Laden der Form "MainView" wird die Controller Instanz der Variablen "con" zugewiesen, sowie dem Controller
+    'die MainView mitgeteilt -gesetzt
     Private Sub MainView_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Me.con = Controller.getInstance
         Me.con.set_MainView(Me)
     End Sub
 
+    'Bei Aufruf dieser Funktion wird die Form "DrawingView" geladen und angezeigt
     Private Sub KonstuierenToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles KonstuierenToolStripMenuItem.Click
         DrawingView.Show()
     End Sub
 
-
-
-    Private Sub Group_1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Group_1.Enter
-
-    End Sub
-
-    'Schnittstelle für Motorcontroller
+    'Diese Funktion setzt die Daten fuer den MotorController 
     Private Sub set_plotter_data(datas As List(Of Integer()))
         moCon.setDatas(datas)
     End Sub
-   
+
+    'Diese Funktion setzt den Pfad der Druckdatei
     Public Sub setFilepath(ByVal filepath As String)
         Me.filepath = filepath
     End Sub
    
-
+    'Bei Klick auf dem Button "Btn_stop" wird diese Methode aufgerufen 
+    'hierbei wird im Controller die Methode "stop_plotter" aufgerufen und somit der Druckvorgang 
+    ' sofort beendet
     Private Sub Btn_stop_Click(sender As Object, e As EventArgs) Handles Btn_stop.Click
         Me.con.stop_plotter()
     End Sub
